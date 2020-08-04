12e7dc0ecc986a41c1546113d9f927e72faf65ed
Naccl <admin@naccl.top>
2020-08-05 05:23:42 +0800

    🙏 使用 semantic-ui 开始前台页面的开发

--- blog-view/src/main.js
+++ blog-view/src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//semantic-ui-vue
+import SuiVue from 'semantic-ui-vue'
+import 'semantic-ui-css/semantic.min.css'
 //moment
 import './util/dateTimeFormatUtils.js'
+
+Vue.use(SuiVue)
 
 
 Vue.config.productionTip = false
