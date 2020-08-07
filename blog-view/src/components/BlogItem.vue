b118dac116301f32a3d51f46cc8e25ebbbb7a6e5
Naccl <admin@naccl.top>
2020-08-07 10:53:04 +0800

    :lipstick: 更新界面和样式文件

--- blog-view/src/components/BlogItem.vue
+++ blog-view/src/components/BlogItem.vue
@@ -5,7 +5,7 @@
 				<div class="ui grid m-margin-lr">
 					<!--标题-->
 					<div class="row m-padded-tb-small">
-						<h2 class="ui header m-center scaleup">
+						<h2 class="ui header m-center m-scaleup">
 							<a href="#" class="m-black">{{ item.title }}</a>
 						</h2>
 					</div>
@@ -15,20 +15,20 @@
 							<div class="item m-datetime">
 								<i class="small calendar icon"></i><span>{{ item.date }}</span>
 							</div>
-							<div class="item m-category">
-								<i class="small folder open icon"></i><a :href="item.id">{{ item.category.name }}</a>
-							</div>
 							<div class="item m-views">
 								<i class="small eye icon"></i><span>{{ item.views }}</span>
 							</div>
-							<div class="item m-words">
+							<div class="item m-common-black">
 								<i class="small pencil alternate icon"></i><span>字数≈{{ item.words }}字</span>
 							</div>
-							<div class="item m-readTime">
+							<div class="item m-common-black">
 								<i class="small clock icon"></i><span>阅读时长≈{{ item.readTime }}分</span>
 							</div>
 						</div>
 					</div>
+					<a :href="item.id" class="ui orange large ribbon label">
+						<i class="small folder open icon"></i><span>{{ item.category.name }}</span>
+					</a>
 					<!--文章描述-->
 					<div class="m-padded-tb-small m-description" v-html="item.description"></div>
 					<!--阅读全文按钮-->
@@ -40,7 +40,7 @@
 					<!--标签-->
 					<div class="row m-padded-tb-no">
 						<div class="column m-padding-left-no">
-							<a :href="tag.id" class="ui mini tag label m-text-500 m-margin-small" :class="tag.color" v-for="tag in item.tags" :key="tag.id">{{ tag.name }}</a>
+							<a :href="tag.id" class="ui tag label m-text-500 m-margin-small" :class="tag.color" v-for="tag in item.tags" :key="tag.id">{{ tag.name }}</a>
 						</div>
 					</div>
 				</div>
@@ -62,5 +62,44 @@
 </script>
 
 <style scoped>
+	.ribbon.label {
+		margin-bottom: 15px !important;
+		left: -46px !important;
+	}
 
+	.m-datetime {
+		color: #00a7e0 !important;
+	}
+
+	.m-datetime * {
+		vertical-align: middle !important;
+	}
+
+	.m-datetime i {
+		margin-right: 3px !important;
+	}
+
+	.m-views {
+		color: #ff3f1f !important;
+	}
+
+	.m-views * {
+		vertical-align: middle !important;
+	}
+
+	.m-views i {
+		margin-right: 3px !important;
+	}
+
+	.m-common-black {
+		color: #000 !important;
+	}
+
+	.m-common-black * {
+		vertical-align: middle !important;
+	}
+
+	.m-common-black i {
+		margin-right: 3px !important;
+	}
 </style>
