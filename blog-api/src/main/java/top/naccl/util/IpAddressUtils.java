4bec5e5b26f3f09a0f625992ebb2245035edcece
Naccl <admin@naccl.top>
2020-12-05 20:46:56 +0800

    解决打包jar后找不到 ip2region.db 的问题

--- blog-api/src/main/java/top/naccl/util/IpAddressUtils.java
+++ blog-api/src/main/java/top/naccl/util/IpAddressUtils.java
@@ -5,9 +5,14 @@ import org.lionsoul.ip2region.DataBlock;
 import org.lionsoul.ip2region.DbConfig;
 import org.lionsoul.ip2region.DbSearcher;
 import org.lionsoul.ip2region.Util;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.util.StringUtils;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
+import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -18,6 +23,7 @@ import java.net.UnknownHostException;
  * @Date: 2020-08-18
  */
 @Slf4j
+@Component
 public class IpAddressUtils {
 	/**
 	 * 在Nginx等代理之后获取用户真实IP地址
@@ -55,6 +61,28 @@ public class IpAddressUtils {
 		return ip;
 	}
 
+	private static DbSearcher searcher;
+	private static Method method;
+
+	/**
+	 * 在服务启动时加载 ip2region.db 到内存中
+	 * 解决打包jar后找不到 ip2region.db 的问题
+	 */
+	@PostConstruct
+	private void initIp2regionResource() {
+		try {
+			InputStream inputStream = new ClassPathResource("/ipdb/ip2region.db").getInputStream();
+			//将 ip2region.db 转为 ByteArray
+			byte[] dbBinStr = FileCopyUtils.copyToByteArray(inputStream);
+			DbConfig dbConfig = new DbConfig();
+			searcher = new DbSearcher(dbConfig, dbBinStr);
+			//二进制方式初始化 DBSearcher，需要使用基于内存的查找算法 memorySearch
+			method = searcher.getClass().getMethod("memorySearch", String.class);
+		} catch (Exception e) {
+			log.error("initIp2regionResource exception:", e);
+		}
+	}
+
 	/**
 	 * 根据ip从 ip2region.db 中获取地理位置
 	 *
@@ -67,10 +95,6 @@ public class IpAddressUtils {
 			return null;
 		}
 		try {
-			DbConfig config = new DbConfig();
-			String dbPath = IpAddressUtils.class.getResource("/ipdb/ip2region.db").getPath();
-			DbSearcher searcher = new DbSearcher(config, dbPath);
-			Method method = searcher.getClass().getMethod("btreeSearch", String.class);
 			DataBlock dataBlock = (DataBlock) method.invoke(searcher, ip);
 			String ipInfo = dataBlock.getRegion();
 			if (!StringUtils.isEmpty(ipInfo)) {
