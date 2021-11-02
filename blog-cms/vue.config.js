7ea20e9a6d42b19a0602fb3d80796964eed0fb67
Naccl <i@naccl.top>
2021-11-02 13:26:14 +0800

    update vue.config.js

--- blog-cms/vue.config.js
+++ blog-cms/vue.config.js
@@ -78,45 +78,5 @@ module.exports = {
 			symbolId: 'icon-[name]'
 		})
 		.end()
-
-		config
-		.when(process.env.NODE_ENV !== 'development',
-			config => {
-				config
-				.plugin('ScriptExtHtmlWebpackPlugin')
-				.after('html')
-				.use('script-ext-html-webpack-plugin', [{
-					// `runtime` must same as runtimeChunk name. default is `runtime`
-					inline: /runtime\..*\.js$/
-				}])
-				.end()
-				config
-				.optimization.splitChunks({
-					chunks: 'all',
-					cacheGroups: {
-						libs: {
-							name: 'chunk-libs',
-							test: /[\\/]node_modules[\\/]/,
-							priority: 10,
-							chunks: 'initial' // only package third parties that are initially dependent
-						},
-						elementUI: {
-							name: 'chunk-elementUI', // split elementUI into a single package
-							priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-							test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-						},
-						commons: {
-							name: 'chunk-commons',
-							test: resolve('src/components'), // can customize your rules
-							minChunks: 3, //  minimum common number
-							priority: 5,
-							reuseExistingChunk: true
-						}
-					}
-				})
-				// https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
-				config.optimization.runtimeChunk('single')
-			}
-		)
 	}
 }
