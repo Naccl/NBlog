e144702bd50bfd8c96c6da97b212d224de960a89
Naccl <i@naccl.top>
2021-11-03 20:36:32 +0800

    fix:header z-index

--- blog-cms/src/layout/index.vue
+++ blog-cms/src/layout/index.vue
@@ -78,7 +78,7 @@ export default {
     position: fixed;
     top: 0;
     right: 0;
-    z-index: 9;
+    z-index: 9999;
     width: calc(100% - #{$sideBarWidth});
     transition: width 0.28s;
   }
--- blog-cms/src/views/login/index.vue
+++ blog-cms/src/views/login/index.vue
@@ -52,8 +52,8 @@
 		data() {
 			return {
 				loginForm: {
-					username: 'Naccl',
-					password: '11'
+					username: '',
+					password: ''
 				},
 				loginRules: {
 					username: [
