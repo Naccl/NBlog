41d6cd126a7baf281768d22153e30e196b043345
Naccl <i@naccl.top>
2021-12-12 09:57:38 +0800

    update

--- blog-api/src/main/java/top/naccl/util/IpAddressUtils.java
+++ blog-api/src/main/java/top/naccl/util/IpAddressUtils.java
@@ -32,7 +32,10 @@ public class IpAddressUtils {
 	 * @return
 	 */
 	public static String getIpAddress(HttpServletRequest request) {
-		String ip = request.getHeader("x-forwarded-for");
+		String ip = request.getHeader("X-Real-IP");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("x-forwarded-for");
+		}
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("Proxy-Client-IP");
 		}
