fbbc002f5bef40d5b721d181803da0ea03369214
Naccl <i@naccl.top>
2023-02-26 18:36:49 +0800

    feat: system settings by db

--- blog-cms/src/views/system/Setting.vue
+++ blog-cms/src/views/system/Setting.vue
@@ -12,7 +12,7 @@
 			<el-tab-pane label="鉴权配置">
 				<el-form label-position="right" label-width="130px">
 					<el-form-item :label="item.nameZh" v-for="item in typeMap.type2" :key="item.id">
-						<el-input v-model="tokenExpireTime" size="mini" v-if="item.nameEn === 'token_expire_time'" @blur="tokenExpireTimeChange" style="width: 100px">
+						<el-input v-model="tokenExpireTime" size="mini" v-if="item.nameEn === 'token.expireTime'" @blur="tokenExpireTimeChange" style="width: 100px">
 							<i slot="suffix" style="font-style: normal; margin-right: 10px">天</i>
 						</el-input>
 						<el-input v-model="item.value" size="mini" v-else></el-input>
@@ -23,8 +23,8 @@
 			<el-tab-pane label="评论配置">
 				<el-form label-position="right" label-width="130px">
 					<el-form-item :label="item.nameZh" v-for="item in typeMap.type3" :key="item.id">
-						<el-switch v-model="item.value" size="mini" v-if="item.nameEn === 'comment_default_open'"></el-switch>
-						<el-select v-model="item.value" size="mini" v-if="item.nameEn === 'comment_notify_channel'" @change="commentNotifyChannelChange">
+						<el-switch v-model="item.value" size="mini" v-if="item.nameEn === 'comment.default-open'"></el-switch>
+						<el-select v-model="item.value" size="mini" v-if="item.nameEn === 'comment.notify.channel '" @change="commentNotifyChannelChange">
 							<el-option label="无" value="none"></el-option>
 							<el-option label="邮箱" value="mail"></el-option>
 							<el-option label="Telegram" value="tg"></el-option>
@@ -35,22 +35,22 @@
 						<el-input v-model="item.value" size="mini"></el-input>
 					</el-form-item>
 
-					<el-form-item :label="item.nameZh" v-for="item in typeMap.type5" :key="item.id" v-if="commentNotifyChannel === 'tg' && ['tg_bot_api', 'tg_bot_token', 'tg_bot_chat_id'].indexOf(item.nameEn) > -1">
+					<el-form-item :label="item.nameZh" v-for="item in typeMap.type5" :key="item.id" v-if="commentNotifyChannel === 'tg' && ['tg.bot.api', 'tg.bot.token', 'tg.bot.chat-id'].indexOf(item.nameEn) > -1">
 						<el-input v-model="item.value" size="mini"></el-input>
 					</el-form-item>
 
-					<el-form-item :label="item.nameZh" v-for="item in typeMap.type5" :key="item.id" v-if="commentNotifyChannel === 'tg' && item.nameEn === 'tg_bot_use_proxy'">
+					<el-form-item :label="item.nameZh" v-for="item in typeMap.type5" :key="item.id" v-if="commentNotifyChannel === 'tg' && item.nameEn === 'tg.bot.use-proxy'">
 						<el-switch v-model="item.value" size="mini" @change="tgBotUseProxyChange"></el-switch>
 					</el-form-item>
 					<el-form-item :label="item.nameZh" v-for="item in typeMap.type5" :key="item.id"
-					              v-if="commentNotifyChannel === 'tg' && tgBotUseProxy && ['tg_bot_proxy_server_host', 'tg_bot_proxy_server_port', 'tg_bot_proxy_server_timeout'].indexOf(item.nameEn) > -1">
+					              v-if="commentNotifyChannel === 'tg' && tgBotUseProxy && ['http.proxy.server.host', 'http.proxy.server.port', 'http.proxy.server.timeout'].indexOf(item.nameEn) > -1">
 						<el-input v-model="item.value" size="mini"></el-input>
 					</el-form-item>
 
-					<el-form-item :label="item.nameZh" v-for="item in typeMap.type5" :key="item.id" v-if="commentNotifyChannel === 'tg' && item.nameEn === 'tg_bot_use_reverse_proxy'">
+					<el-form-item :label="item.nameZh" v-for="item in typeMap.type5" :key="item.id" v-if="commentNotifyChannel === 'tg' && item.nameEn === 'tg.bot.use-reverse-proxy'">
 						<el-switch v-model="item.value" size="mini" @change="tgBotUseReverseProxyChange"></el-switch>
 					</el-form-item>
-					<el-form-item :label="item.nameZh" v-for="item in typeMap.type5" :key="item.id" v-if="commentNotifyChannel === 'tg' && tgBotUseReverseProxy && item.nameEn === 'tg_bot_reverse_proxy_url'">
+					<el-form-item :label="item.nameZh" v-for="item in typeMap.type5" :key="item.id" v-if="commentNotifyChannel === 'tg' && tgBotUseReverseProxy && item.nameEn === 'tg.bot.reverse-proxy-url'">
 						<el-input v-model="item.value" size="mini"></el-input>
 					</el-form-item>
 
@@ -60,7 +60,7 @@
 			<el-tab-pane label="图片存储">
 				<el-form label-position="right" label-width="130px">
 					<el-form-item :label="item.nameZh" v-for="item in typeMap.type6" :key="item.id">
-						<el-select v-model="item.value" size="mini" v-if="item.nameEn === 'upload_channel'" @change="uploadChannelChange">
+						<el-select v-model="item.value" size="mini" v-if="item.nameEn === 'upload.channel'" @change="uploadChannelChange">
 							<el-option label="本地" value="local"></el-option>
 							<el-option label="GitHub" value="github"></el-option>
 							<el-option label="又拍云" value="upyun"></el-option>
@@ -118,22 +118,22 @@ export default {
 		getData() {
 			getSystemSettingData().then(res => {
 				this.typeMap = res.data
-				this.tokenExpireTime = this.findItemByNameEn(2, 'token_expire_time').value / 86400000
+				this.tokenExpireTime = this.findItemByNameEn(2, 'token.expireTime').value / 86400000
 				this.initSwitch()
 			})
 		},
 		initSwitch() {
-			let commentDefaultOpen = this.findItemByNameEn(3, 'comment_default_open')
+			let commentDefaultOpen = this.findItemByNameEn(3, 'comment.default-open')
 			commentDefaultOpen.value = commentDefaultOpen.value === 'true'
 
-			let tgBotUseProxy = this.findItemByNameEn(5, 'tg_bot_use_proxy')
+			let tgBotUseProxy = this.findItemByNameEn(5, 'tg.bot.use-proxy')
 			this.tgBotUseProxy = tgBotUseProxy.value = tgBotUseProxy.value === 'true'
 
-			let tgBotUseReverseProxy = this.findItemByNameEn(5, 'tg_bot_use_reverse_proxy')
+			let tgBotUseReverseProxy = this.findItemByNameEn(5, 'tg.bot.use-reverse-proxy')
 			this.tgBotUseReverseProxy = tgBotUseReverseProxy.value = tgBotUseReverseProxy.value === 'true'
 
-			this.commentNotifyChannel = this.findItemByNameEn(3, 'comment_notify_channel').value
-			this.uploadChannel = this.findItemByNameEn(6, 'upload_channel').value
+			this.commentNotifyChannel = this.findItemByNameEn(3, 'comment.notify.channel').value
+			this.uploadChannel = this.findItemByNameEn(6, 'upload.channel').value
 		},
 		findItemByNameEn(type, nameEn) {
 			for (const item of this.typeMap[`type${type}`]) {
@@ -143,7 +143,7 @@ export default {
 			}
 		},
 		tokenExpireTimeChange() {
-			let tokenExpireTime = this.findItemByNameEn(2, 'token_expire_time')
+			let tokenExpireTime = this.findItemByNameEn(2, 'token.expireTime')
 			if (this.tokenExpireTime <= 0 || this.tokenExpireTime > 365) {
 				this.tokenExpireTime = 1
 				tokenExpireTime.value = 86400000
