dd1b47c2fa51a9c9bb9dd795799f56f554d3860d
chenlong_DJ033979 <raxcl@qq.com>
2024-01-19 13:43:30 +0800

    fix:QQ昵称获取失效的问题修复

---
+++ blog-api/src/main/java/top/naccl/model/vo/QqVO.java
@@ -0,0 +1,30 @@
+package cn.raxcl.model.vo;
+
+import lombok.Data;
+
+/**
+ * @author raxcl
+ * @date 2024-01-19 9:54:53
+ */
+@Data
+public class QqVO {
+    /**
+     * qq号
+     */
+    private Long qq;
+
+    /**
+     * qq昵称
+     */
+    private String name;
+
+    /**
+     * qq邮箱
+     */
+    private String email;
+
+    /**
+     * qq头像
+     */
+    private String avatar;
+}
