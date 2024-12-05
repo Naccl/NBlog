67e74379de394b976fb945ee5affd70c87642b45
mingshuiyi2019 <mingshuiyi@qq.com>
2024-12-05 11:13:00 +0800

    拆包

--- blog-view/vue.config.js
+++ blog-view/vue.config.js
@@ -10,5 +10,29 @@ module.exports = {
 				'plugins': '@/plugins'
 			}
 		}
-	}
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name(module) {
+						// get the name. E.g. node_modules/packageName/not/this/part.js
+						// or node_modules/packageName
+						const packageName = module.context.match(
+							/[\\/]node_modules[\\/](.*?)([\\/]|$)/
+						)[1];
+						// npm package names are URL-safe, but some servers don't like @ symbols
+						return `npm.${packageName.replace("@", "")}`;
+					},
+					chunks: "all",
+					enforce: true,
+					priority: 10,
+					minSize: 50000, // 50KB
+					maxSize: 200000,
+					reuseExistingChunk: true,
+				},
+			},
+		},
+	},
 }
