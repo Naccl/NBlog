3ff5ee10700397e31f7fa8b585f77f549289956d
leijian <@sw-leijian>
2025-02-25 10:40:28 +0800

    test

--- blog-api/src/main/java/top/naccl/BlogApiApplication.java
+++ blog-api/src/main/java/top/naccl/BlogApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogApiApplication {
 
 	public static void main(String[] args) {
+		//test
 		SpringApplication.run(BlogApiApplication.class, args);
 	}
 
